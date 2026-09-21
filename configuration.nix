8930f00d8a359137305d7a3d311569b60e5d1908
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-21 13:43:31 +0530

    add nix helper and clean up comments

--- configuration.nix
+++ configuration.nix
@@ -1,18 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, pkgs, ... }:
 
 {
   imports = [
-    # Include the results of the hardware scan.
     /etc/nixos/hardware-configuration.nix
   ];
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   boot.loader.limine = {
     enable = true;
@@ -24,23 +15,17 @@
     '';
   };
 
-  # Use latest kernel.
+  boot.loader.efi.canTouchEfiVariables = true;
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  hardware.bluetooth.enable = true;
+
   time.timeZone = "Asia/Kolkata";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -55,13 +40,11 @@
     LC_TIME = "en_IN";
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."kaezr" = {
     isNormalUser = true;
     description = "kaezr";
@@ -73,7 +56,6 @@
 
   users.extraUsers."kaezr".shell = pkgs.fish;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   programs.neovim = {
@@ -85,8 +67,6 @@
 
   programs.noctalia.enable = true;
 
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     iputils
     wget
@@ -105,23 +85,19 @@
     bat
     gcc
     tree-sitter
+    lazygit
+    xwayland-satellite
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   programs.niri.enable = true;
   programs.fish.enable = true;
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep 3 --keep-since 3d";
+    flake = "/home/kaezr/.config/nixos";
+  };
 
   services.greetd = {
     enable = true;
@@ -132,6 +108,9 @@
       };
     };
   };
+
+  services.upower.enable = true;
+  services.power-profiles-daemon.enable = true;
 
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
@@ -158,39 +137,11 @@
     };
   };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "26.05"; # Did you read the comment?
-
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+
+  system.stateVersion = "26.05";
 
 }
