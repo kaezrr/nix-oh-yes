2ac787bb73adb004baa7da685c0babc62fa3de8d
Anjishnu Banerjee <kaezr.dev@gmail.com>
2026-09-21 19:59:55 +0530

    add formatter to flake

--- flake.nix
+++ flake.nix
@@ -11,10 +11,15 @@
 
   outputs =
     { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [ ./configuration.nix ];
       };
+      formatter.${system} = pkgs.nixfmt-tree;
     };
 }
